a904524f2833ce1c511e23c572c172b2c8fcd768
Alvaro Mouriño <alvaro@mourino.net>
2012-12-29 03:24:03 +0100

    More backbone

--- shiva/static/shiva.js
+++ shiva/static/shiva.js
@@ -1,6 +1,12 @@
+Shiva = {};
+
 // Base {{{
+buzz.sound.prototype.destroy = function() {
+    this.set('src', '');
+};
+
 Shiva.Collection = Backbone.Collection.extend({
-    server_url: 'http://localhost:5001/api',
+    server_url: '',
     url: function() {
         return this.server_url + this.resource_url;
     },
@@ -8,18 +14,16 @@ Shiva.Collection = Backbone.Collection.extend({
         this.get('player').play();
     },
     stop: function() {
+        this.get('player').destroy();
+    },
+    pause: function() {
         this.get('player').stop();
     }
 });
 
-buzz.sound.prototype.destroy = function() {
-    this.set('src', '');
-}
-
 Shiva.Audio = function(audio_url) {
+    server_url = '';
     return {
-        server_url: 'http://localhost:5001/api',
-
         url: function() {
             return this.server_url + audio_url;
         },
@@ -38,11 +42,45 @@ Shiva.Audio = function(audio_url) {
         },
 
         stop: function() {
-            this.load_track();
+            this.track.destroy();
+        },
+
+        pause: function() {
             this.track.stop();
         }
     }
 }
+
+Shiva.Routes = Backbone.Router.extend({
+    routes: {
+        '': 'artistList',
+        'artists': 'artistList',
+        'artist/:artistId': 'artistDetail'
+    },
+
+    initialize: function(app) {
+        this.app = app;
+    },
+
+    artistList: function() {
+        console.log('artist list');
+        $('#content').html('<ul id="artists"></ul>');
+        app.renderArtists();
+    },
+
+    artistDetail: function(artistId) {
+        console.log('artist detail');
+        $('#content').html('<ul id="albums"></ul>');
+        var artistModel = app.artists.get(artistId);
+        artistModel.each(function(el) {
+            var AlbumView = new Shiva.AlbumView({
+                model: el.get('album'),
+                id: artistModel.get('hash')
+            });
+            $('#albums').append(artistView.render());
+        });
+    }
+});
 // }}}
 
 
@@ -74,6 +112,9 @@ Shiva.Album = Backbone.Model.extend({
         this.set({
             'player': new Shiva.Audio(this.get('download_uri'))
         })
+    },
+    render: function() {
+        return '<li>' + this.model.get('title') + '</li>';
     }
 });
 
@@ -92,5 +133,19 @@ Shiva.Artist = Backbone.Model.extend({
 Shiva.ArtistList = Shiva.Collection.extend({
     model: Shiva.Artist,
     resource_url: '/artists'
+});
+
+Shiva.ArtistView = Backbone.View.extend({
+    el: '#artists',
+    tagName: 'li',
+    className: 'artist',
+    render: function() {
+        var artistId = this.model.get('id');
+        var artistName = this.model.get('name');
+        return '' +
+            '<li class="artist">' +
+                '<a href="#/artist/' + artistId + '" id="artist_' + artistId + '">' + artistName + '</a>' +
+            '</li>';
+    }
 });
 // }}}
