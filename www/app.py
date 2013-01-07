b00c2f61963aed9ea1754da61361483131ef7cce
Alvaro Mouriño <alvaro@mourino.net>
2013-01-07 02:49:17 +0100

    Getting rid of requests

--- www/app.py
+++ www/app.py
@@ -1,33 +1,37 @@
 # -*- coding: utf-8 -*-
 from flask import Flask, Response, render_template
-import requests
 
 import config
 
+import urllib
+
 app = Flask(__name__)
 app.config.from_object(config)
-app.static_folder = 'static'
 
 @app.route('/')
 @app.route('/<path:path>')
 def index(path=None):
-    if not path:
-        response = requests.get(app.config['NODEJS_URL'])
+    if path:
+        uri = '/'.join(((app.config['NODEJS_URL']), path))
     else:
-        response = requests.get('/'.join(((app.config['NODEJS_URL']), path)))
+        uri = app.config['NODEJS_URL']
 
-    return Response(response=response.content, status=response.status_code,
-                    mimetype=response.headers['Content-Type'],
-                    headers=response.headers)
+    request = urllib.urlopen(uri)
+
+    mimetype = request.headers.get('Content-Type', 'text/html')
+    mimetype = mimetype.split(';')[0].strip()
+
+    return Response(request.read(), status=request.getcode(),
+                    mimetype=mimetype)
 
 @app.route('/api/<path:path>')
 def api_call(path):
     """
     """
-    response = requests.get('%s/%s' % (app.config['SHIVA_URL'], path))
+    request = urllib.urlopen('%s/%s' % (app.config['SHIVA_URL'], path))
 
-    return Response(response=response.content, status=response.status_code,
-                    headers=response.headers)
+    return Response(request.read(), status=request.getcode(),
+                    mimetype='application/javascript')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5001, debug=True)
