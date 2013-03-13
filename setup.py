e362f1eb3307f9ee3c1860e8fcc26340e8c62d20
Felix Hummel <felix@felixhummel.de>
2013-03-13 12:12:15 +0100

    setup.py with entry point

---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(name='shiva',
+      version='1.0.0',
+      description='A RESTful API to your music collection',
+      author='Alvaro Mouriño',
+      author_email='alvaro@mourino.net',
+      url='https://github.com/tooxie/shiva-server',
+      package_dir={'':'.'},
+      packages=find_packages('.'),
+      entry_points={
+          'console_scripts': [
+              'shiva-server = shiva.app:main'
+          ]
+      }
+)
+
