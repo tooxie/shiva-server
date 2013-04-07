8cb86d918ec9b3bf616f27bfc4f950731a5348c3
Alvaro Mouriño <alvaro@mourino.net>
2013-04-07 14:25:44 +0200

    Created shiva.mocks module

---
+++ shiva/mocks.py
@@ -0,0 +1,65 @@
+from datetime import datetime
+
+from lxml import etree
+import requests
+
+from shiva.models import Artist
+
+
+class ShowModel(object):
+    """
+    Mock model that encapsulates the show logic for converting a JSON structure
+    into an object.
+
+    """
+
+    def __init__(self, artist, json):
+        self.json = json
+        self.id = json['id']
+        self.artists, self.other_artists = self.split_artists(json['artists'])
+        self.datetime = self.to_datetime(json['datetime'])
+        self.title = json['title']
+        self.tickets_left = (json['ticket_status'] == 'available')
+        self.venue = json['venue']
+
+    def split_artists(self, json):
+        if len(json) == 0:
+            ([], [])
+        elif len(json) == 1:
+            artist = Artist.query.filter_by(name=json[0]['name']).first()
+
+            return ([artist], [])
+
+        my_artists = []
+        other_artists = []
+        for artist_dict in json:
+            artist = Artist.query.filter_by(name=artist_dict['name'])
+            if artist.count():
+                my_artists.append(artist.first())
+            else:
+                del artist_dict['thumb_url']
+                other_artists.append(artist_dict)
+
+        return (my_artists, other_artists)
+
+    def to_datetime(self, timestamp):
+        return datetime.strptime(timestamp, '%Y-%m-%dT%H:%M:%S')
+
+    def get_mbid(self, artist):
+        mb_uri = 'http://musicbrainz.org/ws/2/artist?query=%(artist)s' % {
+            'artist': urllib2.quote(artist)
+        }
+
+        logger.info(mb_uri)
+
+        response = requests.get(mb_uri)
+        mb_xml = etree.fromstring(response.text)
+        # /root/artist-list/artist.id
+        artist_list = mb_xml.getchildren()[0].getchildren()
+        if artist_list:
+            return artist_list[0].get('id')
+
+        return None
+
+    def __getitem__(self, key):
+        return getattr(self, key, None)
--- shiva/resources.py
+++ shiva/resources.py
@@ -5,7 +5,6 @@ import urllib2
 
 from flask import request, current_app as app, g
 from flask.ext.restful import abort, fields, marshal
-from lxml import etree
 import requests
 
 from shiva import get_version, get_contributors
@@ -15,6 +14,7 @@ from shiva.fields import (Boolean, DownloadURI, ForeignKeyField, InstanceURI,
 from shiva.http import Resource, JSONResponse
 from shiva.lyrics import get_lyrics
 from shiva.mimetype import MimeType
+from shiva.mocks import ShowModel
 from shiva.models import Artist, Album, Track, Lyrics
 
 logger = logging.getLogger(__name__)
@@ -485,65 +485,6 @@ class ShowsResource(Resource):
         for event in response.json():
             yield marshal(ShowModel(artist_name, event),
                           self.get_resource_fields())
-
-
-class ShowModel(object):
-    """
-    Mock model that encapsulates the show logic for converting a JSON structure
-    into an object.
-
-    """
-
-    def __init__(self, artist, json):
-        self.json = json
-        self.id = json['id']
-        self.artists, self.other_artists = self.split_artists(json['artists'])
-        self.datetime = self.to_datetime(json['datetime'])
-        self.title = json['title']
-        self.tickets_left = (json['ticket_status'] == 'available')
-        self.venue = json['venue']
-
-    def split_artists(self, json):
-        if len(json) == 0:
-            ([], [])
-        elif len(json) == 1:
-            artist = Artist.query.filter_by(name=json[0]['name']).first()
-
-            return ([artist], [])
-
-        my_artists = []
-        other_artists = []
-        for artist_dict in json:
-            artist = Artist.query.filter_by(name=artist_dict['name'])
-            if artist.count():
-                my_artists.append(artist.first())
-            else:
-                del artist_dict['thumb_url']
-                other_artists.append(artist_dict)
-
-        return (my_artists, other_artists)
-
-    def to_datetime(self, timestamp):
-        return datetime.strptime(timestamp, '%Y-%m-%dT%H:%M:%S')
-
-    def get_mbid(self, artist):
-        mb_uri = 'http://musicbrainz.org/ws/2/artist?query=%(artist)s' % {
-            'artist': urllib2.quote(artist)
-        }
-
-        logger.info(mb_uri)
-
-        response = requests.get(mb_uri)
-        mb_xml = etree.fromstring(response.text)
-        # /root/artist-list/artist.id
-        artist_list = mb_xml.getchildren()[0].getchildren()
-        if artist_list:
-            return artist_list[0].get('id')
-
-        return None
-
-    def __getitem__(self, key):
-        return getattr(self, key, None)
 
 
 class RandomResource(Resource):
