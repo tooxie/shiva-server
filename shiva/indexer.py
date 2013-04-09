86d3282ed6531c6a6d8329ce1c65062bff37612a
Alvaro Mouriño <alvaro@mourino.net>
2013-04-09 15:07:59 +0200

    Function main() for indexer and a .gitignore pattern
    A couple of small fixes that got lost during rebase.

--- shiva/indexer.py
+++ shiva/indexer.py
@@ -190,7 +190,8 @@ class Indexer(object):
             for mdir in mobject.get_valid_dirs():
                 self.walk(mdir)
 
-if __name__ == '__main__':
+
+def main():
     use_lastfm = '--lastfm' in sys.argv
     no_metadata = '--nometadata' in sys.argv
 
@@ -208,3 +209,7 @@ if __name__ == '__main__':
     # Petit performance hack: Every track will be added to the session but they
     # will be written down to disk only once, at the end.
     lola.session.commit()
+
+
+if __name__ == '__main__':
+    main()
