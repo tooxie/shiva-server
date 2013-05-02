7549afc51da69287189d7e71b99ef27017039585
Alvaro Mouriño <alvaro@mourino.net>
2013-05-02 14:11:33 +0200

    Read config from $XDG_CONFIG_HOME/shiva/local.py
    * Read config from shiva/config/local.py
    * Update config with env var SHIVA_CONFIG
    * If not found, look for $XDG_CONFIG_HOME/shiva/local.py file
    
    If all fails, inform the user.

--- shiva/exceptions.py
+++ shiva/exceptions.py
@@ -8,3 +8,15 @@ class InvalidMimeTypeError(Exception):
         msg = "Invalid mimetype '%s'" % str(mimetype)
 
         super(InvalidMimeTypeError, self).__init__(msg)
+
+
+class NoConfigFoundError(Exception):
+    def __init__(self, *args, **kwargs):
+        msg = ('No configuration file found. Please define one:\n'
+               '\t* config/local.py\n'
+               '\t* Set the environment variable $SHIVA_CONFIG pointing to a '
+               'config file.\n'
+               '\t* $XDG_CONFIG_HOME/shiva/config.py which defaults to \n'
+               '\t  $HOME/.config/shiva/config.py')
+
+        super(NoConfigFoundError, self).__init__(msg)
