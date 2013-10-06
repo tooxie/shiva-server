84b307e94382468feed9a0dc4b79f8c77ef340ca
Alvaro Mouriño <alvaro@mourino.net>
2013-10-06 13:17:08 +0200

    Imported resources

---
+++ shiva/resources/__init__.py
@@ -0,0 +1,5 @@
+from shiva.resources.base import ArtistResource, AlbumResource, TrackResource
+from shiva.resources.static import AboutResource, ClientResource
+from shiva.resources.dynamic import (ConvertResource, LyricsResource,
+                                     RandomResource, ShowsResource,
+                                     WhatsNewResource)
