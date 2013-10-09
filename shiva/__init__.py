9f039f4fce8c3852800dff135d37a272815126ed
Alvaro Mouriño <alvaro@mourino.net>
2013-10-09 18:39:40 +0200

    Version bump: 0.8.0

--- shiva/__init__.py
+++ shiva/__init__.py
@@ -37,7 +37,7 @@ to-your-music-collection/
 Copyright © 2013 Alvaro Mouriño
 """
 
-__version__ = '0.7.0'
+__version__ = '0.8.0'
 __author__ = u'Alvaro Mouriño'
 __author_email__ = 'alvaro@mourino.net'
 __copyright__ = '2013 Alvaro Mouriño'
