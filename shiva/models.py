14f6fb5e00f55d3367977560ba01f415fe8c73cb
Alvaro Mouriño <alvaro@mourino.net>
2014-09-02 12:59:53 +0200

    Tracks without album or artist
    Now the fulltree for an artist includes a `no_album_tracks` attribute, which
    contains the list of tracks for that artist that are not related to any album.
    And the `/tracks` resource can retrieve the list of tracks that have no album
    (`/tracks?album=0`), no artist (`/tracks?artist=0`), or neither
    (`/tracks?album=0&artist=0`).

--- shiva/models.py
+++ shiva/models.py
@@ -118,8 +118,6 @@ class Track(db.Model):
     slug = db.Column(db.String(128))
     bitrate = db.Column(db.Integer)
     file_size = db.Column(db.Integer)
-    # TODO could be float if number weren't converted to an int in metadata
-    # manager
     length = db.Column(db.Integer)
     # TODO number should probably be renamed to track or track_number
     number = db.Column(db.Integer)
