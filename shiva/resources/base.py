cd2181a8af052b086d710205a4901f6c7bcb955c
Alvaro Mouriño <alvaro@mourino.net>
2014-09-22 22:59:47 +0200

    Track documentation

--- shiva/resources/base.py
+++ shiva/resources/base.py
@@ -231,7 +231,6 @@ class TrackResource(Resource):
 
         handler.save()
 
-        # TODO: Document this.
         hash_file = parse_bool(request.args.get('hash_file', True))
         no_metadata = parse_bool(request.args.get('no_metadata', False))
 
