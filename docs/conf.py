0ad43d6001df16a57e962c4b9ea48aab50cc1b1b
Alvaro Mouriño <alvaro@mourino.net>
2015-06-27 11:19:15 +0200

    Updated copyright year

--- docs/conf.py
+++ docs/conf.py
@@ -48,7 +48,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Shiva'
-copyright = u'2014, Alvaro Mouriño'
+copyright = u'2015, Alvaro Mouriño'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
